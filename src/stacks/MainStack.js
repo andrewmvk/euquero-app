3723a689e78bb80833a05c09e738f6e72b6531b4
brayan <proffleandro.linux@gmail.com>
2022-06-03 20:15:40 -0300

    Installed dependencies and created page folders

---
+++ src/stacks/MainStack.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Starting from '../screens/Starting';
+import Home from '../screens/Home';
+import AdminLogin from '../screens/AdminLogin';
+import AdminMainMenu from '../screens/AdminMainMenu';
+
+const Stack = createStackNavigator();
+
+export default () => (
+    <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+            headerShown: false
+        }}
+    >
+        <Stack.Screen name="Starting" component={Starting}/>
+        <Stack.Screen name="Home" component={Home}/>
+        <Stack.Screen name="AdminLogin" component={AdminLogin}/>
+        <Stack.Screen name="AdminMainMenu" component={AdminMainMenu}/>
+    </Stack.Navigator>
+)
