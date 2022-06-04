21db1f073bca8b9a8d345fd9b661dbc38e1f5886
brayan <proffleandro.linux@gmail.com>
2022-06-04 18:54:50 -0300

    Home page created

--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,8 +1,78 @@
-import react from "react";
-import styled from 'styled-components/native'
+import styled from "styled-components/native";
 
 export const Container = styled.SafeAreaView`
-    align-items: center;
-    justify-content: center;
-    flex: 1;
+  background: #6a426e;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const LogoView = styled.View`
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 50%;
+`;
+
+export const Subtitle = styled.Text`
+  color: #fff;
+  margin-top: 15px;
+  font-family: "Spartan-Regular";
+  font-size: 14px;
+`;
+
+export const Btns = styled.View`
+  height: 30%;
+  justify-content: center;
+`;
+
+export const BuscarBtn = styled.TouchableOpacity`
+  width: 250px;
+  height: 50px;
+  background-color: #ff6b0f;
+  border-radius: 99px;
+  align-items: center;
+  justify-content: center;
+  padding-bottom: 2px;
+  margin-bottom: 35px;
+`;
+
+export const ComoUsarBtn = styled.TouchableOpacity`
+  width: 200px;
+  height: 40px;
+  background-color: #ff6b0f;
+  border-radius: 20px;
+  align-items: center;
+  justify-content: center;
+  margin-left: 26px;
+  padding-bottom: 2px;
+  margin-bottom: 20px;
+`;
+
+export const SobreProjetoBtn = styled.TouchableOpacity`
+  width: 200px;
+  height: 40px;
+  background-color: #ff6b0f;
+  border-radius: 20px;
+  align-items: center;
+  justify-content: center;
+  margin-left: 26px;
+  padding-bottom: 2px;
+`;
+
+export const TxtBtn = styled.Text`
+  color: #fff;
+  font-family: "Spartan-Regular";
+  font-size: 20px;
+`;
+
+export const TxtBtn2 = styled.Text`
+  color: #fff;
+  font-family: "Spartan-Regular";
+  font-size: 16px;
+`;
+
+export const Icon = styled.View`
+  background-color: #fff;
+  color: blue;
 `;
--- src/stacks/MainStack.js
+++ src/stacks/MainStack.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import Starting from '../screens/Starting';
-import Home from '../screens/Home';
-import AdminLogin from '../screens/AdminLogin';
-import AdminMainMenu from '../screens/AdminMainMenu';
+import Starting from "../screens/Starting";
+import Home from "../screens/Home";
+import AdminLogin from "../screens/AdminLogin";
+import AdminMainMenu from "../screens/AdminMainMenu";
 
 const Stack = createStackNavigator();
 
 export default () => (
   <Stack.Navigator
-    initialRouteName="AdminMainMenu"
+    initialRouteName="Home"
     screenOptions={{
       headerShown: false,
     }}
