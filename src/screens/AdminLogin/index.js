c2d18b0ab4bee092b19a39859ced0a9591bfb8bf
brayan <proffleandro.linux@gmail.com>
2022-06-06 19:11:11 -0300

    Added admin login page and deleted Starting page

--- src/screens/AdminLogin/index.js
+++ src/screens/AdminLogin/index.js
@@ -1,11 +1,46 @@
 import React from "react";
-import { Container } from './styles';
-import { Text } from 'react-native'
+import { View, Dimensions } from "react-native";
+
+import {
+  Container,
+  LogoView,
+  InputArea,
+  Subtitle,
+  CustomButton,
+  CustomButtonText,
+  TextInput,
+} from "./styles";
+import Logo from "../../../assets/images/euquero-logo.svg";
 
 export default () => {
-    return (
-        <Container>
-            <Text>AdminLogin</Text>
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <View
+        style={{
+          alignItems: "center",
+          height: Dimensions.get("window").height * 0.6,
+          justifyContent: "space-evenly",
+        }}
+      >
+        <LogoView>
+          <Logo />
+          <Subtitle>Acesso Administrativo</Subtitle>
+        </LogoView>
+
+        <InputArea>
+          <TextInput
+            placeholder="Nome de usuário"
+            placerholderTextColor="grey"
+          />
+          <TextInput placeholder="Senha" placerholderTextColor="grey" />
+        </InputArea>
+
+        <View style={{ height: "15%" }}>
+          <CustomButton>
+            <CustomButtonText>Acessar</CustomButtonText>
+          </CustomButton>
+        </View>
+      </View>
+    </Container>
+  );
+};
--- src/stacks/MainStack.js
+++ src/stacks/MainStack.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import Starting from '../screens/Starting';
-import Home from '../screens/Home';
-import AdminLogin from '../screens/AdminLogin';
-import AdminMainMenu from '../screens/AdminMainMenu';
+import Starting from "../screens/Starting";
+import Home from "../screens/Home";
+import AdminLogin from "../screens/AdminLogin";
+import AdminMainMenu from "../screens/AdminMainMenu";
 
 const Stack = createStackNavigator();
 
 export default () => (
   <Stack.Navigator
-    initialRouteName="Home"
+    initialRouteName="AdminLogin"
     screenOptions={{
       headerShown: false,
     }}
   >
-    <Stack.Screen name="Starting" component={Starting} />
     <Stack.Screen name="Home" component={Home} />
     <Stack.Screen name="AdminLogin" component={AdminLogin} />
     <Stack.Screen name="AdminMainMenu" component={AdminMainMenu} />
