7585788a1afdb1cd1c2cba3cacac7b9051fd519d
andrewmvk <andrew.medeiros00@gmail.com>
2023-02-18 01:14:51 -0300

    APP android build config

--- src/services/firebase.config.js
+++ src/services/firebase.config.js
@@ -1,21 +1,21 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
-import { REACT_APP_FIREBASE_KEY } from "@env";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+import { REACT_APP_FIREBASE_KEY } from '@env';
 
 const firebaseConfig = {
   apiKey: REACT_APP_FIREBASE_KEY,
-  authDomain: "euquero-app-50e1e.firebaseapp.com",
-  projectId: "euquero-app-50e1e",
-  storageBucket: "euquero-app-50e1e.appspot.com",
-  messagingSenderId: "76643349812",
-  appId: "1:76643349812:web:94d7fa928f00fb5f9f6161",
-  measurementId: "G-ZM8QKEDKE3",
+  authDomain: 'euquero-app-50e1e.firebaseapp.com',
+  projectId: 'euquero-app-50e1e',
+  storageBucket: 'euquero-app-50e1e.appspot.com',
+  messagingSenderId: '76643349812',
+  appId: '1:76643349812:web:94d7fa928f00fb5f9f6161',
+  measurementId: 'G-ZM8QKEDKE3',
 };
 
 const app = initializeApp(firebaseConfig);
-const secondaryApp = initializeApp(firebaseConfig, "Secondary");
+const secondaryApp = initializeApp(firebaseConfig, 'Secondary');
 
 export const auth = getAuth(app);
 export const authSecondary = getAuth(secondaryApp);
